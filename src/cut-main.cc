a00ab6eea158a206879daaae522eb25baf0ad246
Ivan Gankevich <i.gankevich@spbu.ru>
2020-12-23 16:02:09 +0300

    Better error handling.

--- src/cut-main.cc
+++ src/cut-main.cc
@@ -1,6 +1,7 @@
 #include <cstring>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <system_error>
 #include <vector>
@@ -8,7 +9,7 @@
 #include "config.h"
 
 void print_usage(const char* name) {
-    printf("usage: %s [-h] [--help] [--version] [--confirm] [blacklist-file]\n", name);
+    printf("usage: %s [-h] [--help] [--version] [--confirm] [diff-file]\n", name);
 }
 
 void print_version() {
@@ -21,24 +22,28 @@ int main(int argc, char* argv[]) {
     if (argc <= 1) { print_usage(argv[0]); std::exit(1); }
     if (argc == 2 && std::strcmp(argv[1], "--version") == 0) { print_version(); exit(0); }
     if (argc == 2) {
-       if (std::strcmp(argv[1], "--confirm") == 0) { confirmed = true; }
-       else { diff_path = argv[1]; }
+        if (std::strcmp(argv[1], "--confirm") == 0) { confirmed = true; }
+        else { diff_path = argv[1]; }
     }
     if (argc == 3) {
-       if (std::strcmp(argv[1], "--confirm") == 0) {
-           confirmed = true;
-           diff_path = argv[2];
-       } else
-       if (std::strcmp(argv[2], "--confirm") == 0) {
-           confirmed = true;
-           diff_path = argv[1];
-       } else {
-           print_usage(argv[0]);
-           std::exit(1);
-       }
+        if (std::strcmp(argv[1], "--confirm") == 0) {
+            confirmed = true;
+            diff_path = argv[2];
+        } else if (std::strcmp(argv[2], "--confirm") == 0) {
+            confirmed = true;
+            diff_path = argv[1];
+        } else {
+            print_usage(argv[0]);
+            std::exit(1);
+        }
     }
     std::ifstream in;
     in.open(diff_path);
+    if (!in.is_open()) {
+        std::stringstream msg;
+        msg << "failed to open " << diff_path;
+        throw std::runtime_error(msg.str());
+    }
     in.exceptions(std::ios::badbit | std::ios::failbit);
     std::vector<std::string> files;
     std::string line;
@@ -48,13 +53,13 @@ int main(int argc, char* argv[]) {
     in.close();
     if (!confirmed) {
         std::string answer;
-        while (answer == "y" || answer == "n") {
+        do {
             std::cout
                 << "This command will delete " << files.size()
                 << " file(s). Are you sure? [y/N]" << std::endl;
             std::cin >> answer;
             for (auto& ch : answer) { ch = std::tolower(ch); }
-        }
+        } while (answer == "y" || answer == "n");
         if (answer == "y") { confirmed = true; }
     }
     if (confirmed) {
